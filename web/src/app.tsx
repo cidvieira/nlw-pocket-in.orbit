bcec3425449b338a69c66f9c7d1595b7e28369da
Cid Vieira <cidvieira99@gmail.com>
2025-11-20 08:35:49 -0300

    feat: frontend project in.orbit

---
+++ web/src/app.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+import { Dialog } from './components/ui/dialog'
+import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
+import { CreateGoal } from './components/create-goal'
+
+export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
+
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      <CreateGoal />
+    </Dialog>
+  )
+}
